912ce4db0d77710fed34fbbcc8de7c8564b12e4c
Alex <alexnossofff@gmail.com>
2019-10-10 22:41:20 +0300

    Clean Django project added (+ 2 models)

--- meeting_registration/meeting_reg/models.py
+++ meeting_registration/meeting_reg/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 
+class Student(models.Model):
+    student_name = models.CharField(max_length=100)
+    relative_name = models.CharField(max_length=100)
+    grade = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
 class Teachers(models.Model):
-    name = models.CharField()
-    list_of_grades = models.CharField()
-    cabinet = models.CharField()
-    phone_number = models.CharField()
+    name = models.CharField(max_length=100)
+    list_of_grades = models.CharField(max_length=100)
+    cabinet = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
     email = models.EmailField()
     number_of_visitors = models.IntegerField()
-    workload = models.IntegerField() #Three levels: 0 (<= 5), 1 (<=10), 2 (<=15)
+    list_of_visitors = models.ManyToManyField(Student)
+    workload = models.IntegerField()  # Three levels: 0 (<= 5), 1 (<=10), 2 (<=15)
+
+
--- meeting_registration/meeting_registration/settings.py
+++ meeting_registration/meeting_registration/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'meeting_reg.apps.MeetingRegConfig'
 ]
 
 MIDDLEWARE = [
