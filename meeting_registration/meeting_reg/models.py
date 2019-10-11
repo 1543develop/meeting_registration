8b64f8a59f91e20c8ce339bc9bef8509ab3bc3a4
tirlimster <tirlimster@gmail.com>
2019-10-11 15:46:43 +0300

    Добавил конфиг (+поменял .gitignore) и админку.

--- meeting_registration/meeting_reg/models.py
+++ meeting_registration/meeting_reg/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 
 class Student(models.Model):
-    student_name = models.CharField(max_length=100)
-    relative_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    relative = models.CharField(max_length=100)
     grade = models.CharField(max_length=10)
     phone_number = models.CharField(max_length=100)
     email = models.EmailField()
 
+    def __str__(self):
+        return f"{self.name} {self.grade}"
 
-class Teachers(models.Model):
+
+class Teacher(models.Model):
     name = models.CharField(max_length=100)
     list_of_grades = models.CharField(max_length=100)
     cabinet = models.CharField(max_length=100)
@@ -19,4 +22,5 @@ class Teachers(models.Model):
     list_of_visitors = models.ManyToManyField(Student)
     workload = models.IntegerField()  # Three levels: 0 (<= 5), 1 (<=10), 2 (<=15)
 
-
+    def __str__(self):
+        return f"{self.name} {self.list_of_grades}"
